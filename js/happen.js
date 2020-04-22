e5318a462eabeb3c25f135aacddea294652cf10e
Arlan95220 <61817289+Arlan95220@users.noreply.github.com>
2020-04-22 10:27:35 +0800

    Update happen.js

--- js/happen.js
+++ js/happen.js
@@ -1,9 +1,9 @@
 var imgs = new Array
 (
-"../image/activity1.jpg",
 "../image/ima1.jpg",
 "../image/2.jpg",
 "../image/ima3.jpg",
+"../image/activity1.jpg",
 )
 var nums =0;
 setInterval("fort()",4000);
